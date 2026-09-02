45c98ee1249e9e5840e3e0aaff1edef08cd75cee
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-09-02 19:01:08 -0300

    feat: implement auth filter

--- src/main/java/com/example/projeto_pdm_artesao_api/config/SecurityConfig.java
+++ src/main/java/com/example/projeto_pdm_artesao_api/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.example.projeto_pdm_artesao_api.config;
 
+import com.example.projeto_pdm_artesao_api.repositories.ArtesaoRepository;
+import com.example.projeto_pdm_artesao_api.service.JwtService;
+import com.example.projeto_pdm_artesao_api.security.JwtAuthenticationFilter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -14,15 +23,47 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    @Bean // open auth routes publicly (test api)
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+            JwtService jwtService,
+            ArtesaoRepository artesaoRepository
+    ) {
+        return new JwtAuthenticationFilter(
+                jwtService,
+                artesaoRepository
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter
+    ) throws Exception {
+
         http
             .csrf(csrf -> csrf.disable())
+
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(
+                    SessionCreationPolicy.STATELESS
+                )
+            )
+
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+
+                .requestMatchers(
+                    "/api/auth/**"
+                ).permitAll()
+
                 .anyRequest().authenticated()
+            )
+
+            .addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
             );
+
         return http.build();
-}
+    }
 }
